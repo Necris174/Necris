22535c4d600373787094baee2536f378aaf11f46
Владимир <plotnicovvyu@gmail.com>
2021-03-09 07:16:37 +0500

    Map1

--- app/src/main/java/ru/locarus/androidtrackerapp/MapsActivity.java
+++ app/src/main/java/ru/locarus/androidtrackerapp/MapsActivity.java
@@ -11,6 +11,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -19,16 +20,28 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CustomCap;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private Double lat = 55.189769;
     private Double lon = 61.365417;
     private Point point;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    List <LatLng> latlnglist = new ArrayList<>();
+
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
@@ -52,7 +65,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         if(actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
+        sharedPreferences = this.getSharedPreferences("LatLng", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
         LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
                 new IntentFilter("custom"));
 
@@ -62,15 +76,95 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         public void onReceive(Context context, Intent intent) {
             // Get extra data included in the Intent
                  point = (Point) intent.getExtras().getSerializable("point");
-                 LatLng sydney = new LatLng(point.getLatitude(), point.getLongitude());
                  lat = point.getLatitude();
                  lon = point.getLongitude();
+                 Log.d("Array", "Size" + latlnglist.size() + "");
+                 if (latlnglist.size()>2){
+                     latlnglist.remove(0);
+                     latlnglist.add(new LatLng(lat,lon));
+                     Log.d("Array", "Array: " + latlnglist.get(0) + latlnglist.get(1) + latlnglist.get(2));
+                 }
+                 else {
+                     latlnglist.add(new LatLng(lat,lon));
+
+                     Log.d("Array", "Array 1:  " + latlnglist.toString());
+                 }
+
+                 switch (latlnglist.size()){
+                     case (1):
+
+                         drawPoint();
+                         break;
+                     case (2):
+                         if(latlnglist.get(0).equals(latlnglist.get(1))){
+                             drawPoint();
+                         } else {
+                             Log.d("Array", "Рисуем 2 точки");
+                             drawTrek();
+                         }
+                         break;
+                     case (3):
+                         if(latlnglist.get(0).equals(latlnglist.get(1))&&latlnglist.get(1).equals(latlnglist.get(2))){
+                             drawPoint();
+                         } else {
+                             Log.d("Array", "Рисуем 3 точки");
+                             drawTrek();
+                         }
+                         drawTrek();
+                         break;
+                 }
+//               if (latlnglist.size()>2) {
+//                   mMap.clear();
+//                   Polyline polyline1 = mMap.addPolyline(new PolylineOptions()
+//                           .clickable(true)
+//                           .addAll(latlnglist));
+//                   polyline1.setTag("tes1");
+//                   polyline1.setEndCap(new CustomCap(BitmapDescriptorFactory.fromResource(R.drawable._34324)));
+//                   polyline1.setWidth(12);
+//                   polyline1.setColor(0xff000000);
+//                   polyline1.setJointType(JointType.ROUND);
+//
+//                   // Position the map's camera near Alice Springs in the center of Australia,
+//                   // and set the zoom factor so most of Australia shows on the screen.
+//                   mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlnglist.get(0), 12));
+//               } else {
+//                   mMap.clear();
+//                   mMap.addMarker(new MarkerOptions()
+//                           .position(latlnglist.get(0))
+//                           .title("Marker in Sydney")
+//                           .icon(BitmapDescriptorFactory.fromResource(R.drawable.pngwing)));
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlnglist.get(0),12f));
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(latlnglist.get(0)));
+//               }
 //                 mMap.clear();
 //            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 //            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,12f));
 //            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
            }
     };
+
+     public void drawTrek (){
+         mMap.clear();
+         Polyline polyline1 = mMap.addPolyline(new PolylineOptions()
+                 .clickable(true)
+                 .addAll(latlnglist));
+         polyline1.setTag("tes1");
+         polyline1.setEndCap(new CustomCap(BitmapDescriptorFactory.fromResource(R.drawable._34324)));
+         polyline1.setWidth(12);
+         polyline1.setColor(0xff000000);
+         polyline1.setJointType(JointType.ROUND);
+         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlnglist.get(0), 12));
+     }
+     public void drawPoint(){
+         Log.d("Array", "Рисуем точку");
+        mMap.clear();
+         mMap.addMarker(new MarkerOptions()
+                 .position(latlnglist.get(0))
+                 .title("Marker in Sydney")
+                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.pngwing)));
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(latlnglist.get(0)));
+         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlnglist.get(0),12f));
+    }
 
     @Override
     protected void onDestroy() {
@@ -90,31 +184,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Log.d("receiver", "Got map: " + lat +" "+ lon);
-        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
-                .clickable(true)
-                .add(
-                        new LatLng(-35.016, 143.321),
-                        new LatLng(-34.747, 145.592),
-                        new LatLng(-34.364, 147.891),
-                        new LatLng(-33.501, 150.217),
-                        new LatLng(-32.306, 149.248),
-                        new LatLng(-32.491, 147.309)));
-        polyline1.setTag("test");
-
-        // Position the map's camera near Alice Springs in the center of Australia,
-        // and set the zoom factor so most of Australia shows on the screen.
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-23.684, 133.903), 4));
-
-        if (lat!=null&&lon!=null) {
-            // Add a marker in Sydney and move the camera
-//            LatLng sydney = new LatLng(lat, lon);
-//            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,12f));
-//            mMap.addMarker(new MarkerOptions().position(sydney).title("1111111111"));
-
-
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(lat,lon))
+                .title("Marker in Sydney")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.pngwing)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lat,lon)));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat,lon),12));
 
         }
-    }
+
 }
